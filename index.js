6a9ee6122d7fb6bd7a3fdfeb904cc7cf760dabfa
Nirupama Dora <nirupamadora71694@gmail.com>
2024-01-30 21:58:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express")
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const  username = process.env.MONGODB_USERNAME;
+const  password = process.env.MONGODB_PASSWORD;
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.gsmlpd5.mongodb.net/registrationFormDB`,{
+    // useNewUrlParser : true,
+    // useUnifiedTopology : true,
+});
+const registrationSchema = new mongoose.Schema({
+    name:String,
+    email: String,
+    password :String
+});
+const Registrartion = mongoose.model("Registration",registrationSchema);
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/pages/index.html");
+})
+app.post("/register", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        // Move the declaration of registrationData above its usage
+        const registrationData = await Registrartion.findOne({ email: email });
+
+        if (!registrationData) {
+            const newRegistrationData = new Registrartion({
+                name,
+                email,
+                password
+            });
+
+            await newRegistrationData.save();
+            res.redirect("/success");
+        } else {
+            console.log("User already exists");
+            res.redirect("/error");
+        }
+    } catch (error) {
+        console.log(error);
+        res.redirect("/error");
+    }
+});
+
+
+app.get("/success",(req,res)=>{
+    res.sendFile(__dirname+"/pages/success.html");
+})
+app.get("/error",(req,res)=>{
+    res.sendFile(__dirname+"/pages/error.html");
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
